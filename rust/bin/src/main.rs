f5ecb5121cac9460383d3a4841ba5e7634a14df9
Jack O'Connor <oconnor663@gmail.com>
2021-12-27 20:23:39 -0500

    add hex support for decryption, and -z short for --zero-key

--- rust/bin/src/main.rs
+++ rust/bin/src/main.rs
@@ -15,8 +15,16 @@ fn parse_args() -> clap::ArgMatches<'static> {
                 .long(DECRYPT_ARG)
                 .short("d"),
         )
-        .arg(clap::Arg::with_name(HEX_ARG).long(HEX_ARG))
-        .arg(clap::Arg::with_name(ZERO_KEY_ARG).long(ZERO_KEY_ARG))
+        .arg(
+            clap::Arg::with_name(HEX_ARG)
+                .long(HEX_ARG)
+                .help("either encrypt to hex, or decrypt from hex"),
+        )
+        .arg(
+            clap::Arg::with_name(ZERO_KEY_ARG)
+                .long(ZERO_KEY_ARG)
+                .short("z"),
+        )
         .get_matches()
 }
 
@@ -26,12 +34,42 @@ struct HexWriter<W> {
 
 impl<W: Write> Write for HexWriter<W> {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        // Quick and dirty. A more serious implementation would avoid heap allocation entirely.
         self.inner_writer.write_all(hex::encode(buf).as_bytes())?;
         Ok(buf.len())
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
         self.inner_writer.flush()
+    }
+}
+
+struct HexReader<R> {
+    inner_reader: R,
+}
+
+impl<R: Read> Read for HexReader<R> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        // Quick and dirty. A more serious implementation would avoid heap allocation entirely.
+        let hex_buf_size = std::cmp::min(65536, buf.len().checked_mul(2).unwrap());
+        let mut hex_buf = Vec::with_capacity(hex_buf_size);
+        self.inner_reader
+            .by_ref()
+            .take(hex_buf_size as u64)
+            .read_to_end(&mut hex_buf)?;
+        debug_assert_eq!(hex_buf.capacity(), hex_buf_size);
+        // Strip trailing whitespace.
+        while hex_buf
+            .last()
+            .map(|c| c.is_ascii_whitespace())
+            .unwrap_or(false)
+        {
+            hex_buf.pop();
+        }
+        let decoded = hex::decode(&hex_buf)
+            .map_err(|_| std::io::Error::new(std::io::ErrorKind::InvalidData, "invalid hex"))?;
+        buf[..decoded.len()].copy_from_slice(&decoded);
+        Ok(decoded.len())
     }
 }
 
@@ -59,15 +97,23 @@ fn main() {
     let stdin = std::io::stdin();
     let stdout = std::io::stdout();
 
-    let input: &mut dyn Read = &mut stdin.lock();
+    let mut input: &mut dyn Read = &mut stdin.lock();
     let mut output: &mut dyn Write = &mut stdout.lock();
 
-    let mut hexer;
+    let mut hex_writer;
+    let mut hex_reader;
     if args.is_present(HEX_ARG) {
-        hexer = HexWriter {
-            inner_writer: output,
-        };
-        output = &mut hexer;
+        if args.is_present(DECRYPT_ARG) {
+            hex_reader = HexReader {
+                inner_reader: input,
+            };
+            input = &mut hex_reader;
+        } else {
+            hex_writer = HexWriter {
+                inner_writer: output,
+            };
+            output = &mut hex_writer;
+        }
     }
 
     if args.is_present(DECRYPT_ARG) {
@@ -76,7 +122,7 @@ fn main() {
         encrypt(key_array, input, output);
     }
 
-    if args.is_present(HEX_ARG) {
+    if args.is_present(HEX_ARG) && !args.is_present(DECRYPT_ARG) {
         println!();
     }
 }
